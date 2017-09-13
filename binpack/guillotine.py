fa78241f3b6e3ab5192c52433b9751f57ca3d1f8
Solomon Bothwell <ssbothwell@gmail.com>
2017-09-12 20:49:06 -0700

    Guillotine: added worst fits

--- binpack/guillotine.py
+++ binpack/guillotine.py
@@ -95,15 +95,202 @@ class Guillotine:
 
 
     def best_height_fit(self, item) -> bool:
-        pass
+        best = None
+        for freerect in self.freerects:
+            if item.x <= freerect.width and item.y <= freerect.height:
+                if not best:
+                    best = freerect
+                elif freerect.height < best.height:
+                    best = freerect
+
+        if best:
+            item.CornerPoint = (freerect.x, freerect.y)
+            self.items.append(item)
+            self.freerects.remove(best)
+            if item.x < freerect.width:
+                # generate free rectangle for remaining width
+                right_width = freerect.width - item.x
+                right_height = item.y
+                right_x = freerect.x + item.x
+                right_y = freerect.y
+                right_rect = FreeRectangle(right_width,
+                                           right_height,
+                                           right_x,
+                                           right_y)
+                self.freerects.append(right_rect)
+            if item.y < freerect.height:
+                top_width = freerect.width
+                top_height = freerect.height - item.y
+                top_x = freerect.x
+                top_y = item.y
+                top_rect = FreeRectangle(top_width,
+                                         top_height,
+                                         top_x,
+                                         top_y)
+                self.freerects.append(top_rect)
+            print(best, item)
+            return True
+        return False
 
 
     def best_area_fit(self, item) -> bool:
-        pass
+        best = None
+        best_area = None
+        for freerect in self.freerects:
+            if item.x <= freerect.width and item.y <= freerect.height:
+                if not best:
+                    best = freerect
+                    best_area = best.width * best.height
+                elif (freerect.height * freerect.width) < best_area:
+                    best = freerect
+
+        if best:
+            item.CornerPoint = (freerect.x, freerect.y)
+            self.items.append(item)
+            self.freerects.remove(best)
+            if item.x < freerect.width:
+                # generate free rectangle for remaining width
+                right_width = freerect.width - item.x
+                right_height = item.y
+                right_x = freerect.x + item.x
+                right_y = freerect.y
+                right_rect = FreeRectangle(right_width,
+                                           right_height,
+                                           right_x,
+                                           right_y)
+                self.freerects.append(right_rect)
+            if item.y < freerect.height:
+                top_width = freerect.width
+                top_height = freerect.height - item.y
+                top_x = freerect.x
+                top_y = item.y
+                top_rect = FreeRectangle(top_width,
+                                         top_height,
+                                         top_x,
+                                         top_y)
+                self.freerects.append(top_rect)
+            print(best, item)
+            return True
+        return False
 
 
     def worst_width_fit(self, item) -> bool:
-        pass
+        best = None
+        for freerect in self.freerects:
+            if item.x <= freerect.width and item.y <= freerect.height:
+                if not best:
+                    best = freerect
+                elif freerect.width > best.width:
+                    best = freerect
+
+        if best:
+            item.CornerPoint = (freerect.x, freerect.y)
+            self.items.append(item)
+            self.freerects.remove(best)
+            if item.x < freerect.width:
+                # generate free rectangle for remaining width
+                right_width = freerect.width - item.x
+                right_height = item.y
+                right_x = freerect.x + item.x
+                right_y = freerect.y
+                right_rect = FreeRectangle(right_width,
+                                           right_height,
+                                           right_x,
+                                           right_y)
+                self.freerects.append(right_rect)
+            if item.y < freerect.height:
+                top_width = freerect.width
+                top_height = freerect.height - item.y
+                top_x = freerect.x
+                top_y = item.y
+                top_rect = FreeRectangle(top_width,
+                                         top_height,
+                                         top_x,
+                                         top_y)
+                self.freerects.append(top_rect)
+            print(best, item)
+            return True
+        return False
+
+
+    def worst_height_fit(self, item) -> bool:
+        best = None
+        for freerect in self.freerects:
+            if item.x <= freerect.width and item.y <= freerect.height:
+                if not best:
+                    best = freerect
+                elif freerect.height > best.height:
+                    best = freerect
+
+        if best:
+            item.CornerPoint = (freerect.x, freerect.y)
+            self.items.append(item)
+            self.freerects.remove(best)
+            if item.x < freerect.width:
+                # generate free rectangle for remaining width
+                right_width = freerect.width - item.x
+                right_height = item.y
+                right_x = freerect.x + item.x
+                right_y = freerect.y
+                right_rect = FreeRectangle(right_width,
+                                           right_height,
+                                           right_x,
+                                           right_y)
+                self.freerects.append(right_rect)
+            if item.y < freerect.height:
+                top_width = freerect.width
+                top_height = freerect.height - item.y
+                top_x = freerect.x
+                top_y = item.y
+                top_rect = FreeRectangle(top_width,
+                                         top_height,
+                                         top_x,
+                                         top_y)
+                self.freerects.append(top_rect)
+            print(best, item)
+            return True
+        return False
+
+
+    def worst_area_fit(self, item) -> bool:
+        best = None
+        best_area = None
+        for freerect in self.freerects:
+            if item.x <= freerect.width and item.y <= freerect.height:
+                if not best:
+                    best = freerect
+                    best_area = best.width * best.height
+                elif (freerect.height * freerect.width) > best_area:
+                    best = freerect
+
+        if best:
+            item.CornerPoint = (freerect.x, freerect.y)
+            self.items.append(item)
+            self.freerects.remove(best)
+            if item.x < freerect.width:
+                # generate free rectangle for remaining width
+                right_width = freerect.width - item.x
+                right_height = item.y
+                right_x = freerect.x + item.x
+                right_y = freerect.y
+                right_rect = FreeRectangle(right_width,
+                                           right_height,
+                                           right_x,
+                                           right_y)
+                self.freerects.append(right_rect)
+            if item.y < freerect.height:
+                top_width = freerect.width
+                top_height = freerect.height - item.y
+                top_x = freerect.x
+                top_y = item.y
+                top_rect = FreeRectangle(top_width,
+                                         top_height,
+                                         top_x,
+                                         top_y)
+                self.freerects.append(top_rect)
+            print(best, item)
+            return True
+        return False
 
 
     def __insert__(self, item: Item) -> bool:
